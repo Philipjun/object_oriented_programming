ae5e6936fce4224af7e94079cd3f6dc96b60dbde
Philip Jun <inbox@philipjun.com>
2014-03-27 17:59:01 -0400

    commander

--- commander.rb
+++ commander.rb
@@ -1,29 +1,32 @@
-require_relative "marsrover"
+require './rover'
+
 # 5 5
 # 1 2 N
 # LMLMLMLMM
 
 class Commander
-	
-	def create_rover(starting_point)
-		coords = starting_point.split(" ")
-		@rover = Rover.new(coords[0], coords[1], coords[2])
-	end
+  X = 0
+  Y = 1
+  DIRECTION = 2
 
-	def move_mover(move_command)
-		move_command.split("").each do |c| 
-			if v =="L"
-				@rover.turn_left
-			elsif c == "R"
-				@rover.turn_right
-			elsif c == "M"
-				@rover.move
+  def create_rover(starting_point)
+    coords = starting_point.split(" ")
+    @rover = Rover.new(coords[X], coords[Y], coords[DIRECTION])
+  end
 
-		end		
-		end
-end
-
+  def move_rover(move_command)
+    move_command.split("").each do |c|
+      if c == "L"
+        @rover.turn_left
+      elsif c == "R"
+        @rover.turn_right
+      elsif c == "M"
+        @rover.move
+      end
+    end
+  end
 end
 
 command = Commander.new
 command.create_rover("1 2 N")
+
