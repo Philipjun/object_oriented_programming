2e186786ef99419b40afb62e2ec82356b870bfd6
Philip Jun <inbox@philipjun.com>
2014-03-27 23:57:29 -0400

    mars updated

--- marsrover.rb
+++ marsrover.rb
@@ -1,18 +1,27 @@
 class Rover
 
+	# why is this necessary?
+	# what are my instance variables?
+	# what ARE instance variables?
+		# how are they different from 'normal' variables?
+
+	# what's an instance of a class? (an object)
+	# how do i make an instance of a class (Rover.new)
+
+
+attr_accessor :x, :y, :direction
+
 	DIRECTIONS = %w[N E S W]
 	NORTH = 0
 	EAST = 1
 	SOUTH = 2
 	WEST = 3
 
-	MOVES = {}
-	MOVES[NORTH] =''
-
 	def initialize (x,y,direction)
 		@x = x
 		@y = y
 		@direction = direction
+
 	end
 
 	def move
@@ -49,6 +58,9 @@ class Rover
 			@direction = "N"
 		end
 	end
-
 end
+
+
+Phil = Rover.new(1, 2, 'N')
+Phil.move
 
